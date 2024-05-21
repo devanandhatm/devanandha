9bc27ae4b51150c88ca689e9cf2093db163108e1
devanandhatm <150226332+devanandhatm@users.noreply.github.com>
2024-05-21 14:56:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+//  console.log("hello world!!");
+// let a=1;
+// let b=2;
+// console.log(a+b);
+// let a=10;
+// let b=20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// postfix incrememt
+// let a=1;
+// let b=a++;
+// console.log(a);
+// console.log(b);
+// // prefix increment
+// let c=1;
+// let d=++c;
+// console.log(c);
+// console.log(d);
+// let car={
+//     model:"celerio",
+//     colour:"black"
+// };
+// console.log(car);
+// let persons={
+//     fname:"devamamdha tm",
+//     address:{
+//         street:"abc",
+//         place:"xyz",
+//         pin:123
+//     },
+//     age:17
+// }
+// console.log(persons.address.place);
+// console.log(persons.age);
+// var Toothbrush=new Object();
+// Toothbrush.brand="close up";
+// Toothbrush.color="black";
+// console.log(toothbrush);
+// concatination
+// let fname = "DEVANANDHA";
+// let lname = " T M ";
+// console.log(`${fname}${lname}`)
+// let m=5;
+// let n=6;
+// console.log(`the sum is${m+n}`);
+
+// Array
+
+// let fruits=["Apple","mango","orange",23]
+// console.log(fruits);
+// fruits.push("grapes")
+// console.log(fruits);
+// console.log(fruits.length);
+// fruits[2]="pineapple";
+// console.log(fruits);
+// fruits.unshift("pear");
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+
+// fruits.shift();
+// console.log(fruits);
+
+// Array of the object
+
+
+function add(a,b){
+    let c = a+b;
+    console.log(c);
+}
+add(1,2);
